06b049cec389e4b49705c411e10e3fcc9c4b2c00
ChristianReyes <christianquimporeyes@gmail.com>
2021-09-12 21:46:07 +0800

    final

---
+++ src/views/Home.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="home">
+    <section class="hero">
+      <div class="hero-text container">
+        <h4>Vue Responsive Nav Menu</h4>
+        <hr />
+        <h2>
+          With Vue <br />
+          Animations
+        </h2>
+        <hr />
+      </div>
+    </section>
+    <section>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis sequi debitis voluptates nesciunt impedit harum iure, provident odit doloremque aliquam temporibus quo omnis error obcaecati rem tempora quasi recusandae necessitatibus ab magnam voluptatibus illum itaque consectetur! Nisi eaque libero delectus necessitatibus voluptatem iste id, omnis dolores iure nulla! Obcaecati sequi ipsa vitae ducimus blanditiis eveniet dolorum corporis labore? Adipisci, voluptatibus omnis quis itaque quae id, laudantium voluptatem quas odit sunt ea animi modi magnam, perspiciatis commodi laboriosam accusantium quidem! Asperiores, cupiditate quos amet numquam eius temporibus eveniet aliquid consequuntur nulla architecto illum quidem iure similique accusamus non placeat error quo, excepturi qui, modi et vitae! Molestias quibusdam repellat ipsa facilis nesciunt adipisci quae praesentium sint odit blanditiis. Quam nostrum minima odio facilis, cum non, asperiores fugit consequatur ex vero iste est incidunt maiores laborum eaque magnam! Magnam accusantium assumenda distinctio doloremque libero. Asperiores veniam id quas culpa voluptate eum ad consequuntur voluptas. Laboriosam, hic delectus? Accusamus consectetur tempora voluptates sint porro? Quas veniam nisi fuga sunt vitae sit maiores autem in molestiae minus, tempora illo, quod consectetur! Aut blanditiis natus quisquam delectus cum expedita quos sit, fuga deleniti molestiae illum ut aspernatur commodi suscipit facilis. Obcaecati, rem facere placeat molestiae libero accusantium! Laboriosam totam eveniet iste voluptate velit? Consectetur doloremque, excepturi exercitationem qui quam ipsa culpa voluptate esse magnam officia perspiciatis suscipit placeat alias! Aut, adipisci ipsa! Adipisci, odio? A, quam nihil inventore aperiam eius earum laborum numquam doloremque quod, totam quisquam, porro dolore tempore quo ducimus ipsam laudantium cumque dolorum reiciendis sit culpa maiores minus? Maxime itaque maiores impedit quas aut doloribus possimus minima, pariatur eos temporibus quisquam quis consectetur omnis nihil saepe. Optio, quos necessitatibus mollitia nisi ab ut quas cumque ullam ducimus beatae veniam velit eius repudiandae deserunt aspernatur aliquid reprehenderit tempore facere fuga, dicta minima exercitationem harum molestias excepturi. Hic, ab, nam magnam ipsam soluta tempora alias quam inventore suscipit perferendis libero eum odio, a distinctio eveniet magni ex iure dicta modi quasi voluptas! Et sint ratione molestias expedita omnis culpa autem? Magnam soluta delectus consequuntur ad nemo in eius velit nisi consequatur totam iste quisquam omnis repellat sint cupiditate, maiores aliquid odio enim maxime perspiciatis? Maxime tenetur nemo a fuga earum aliquid corrupti aspernatur nisi eveniet. Fuga alias natus aspernatur distinctio incidunt dolorem accusantium nisi, facilis consectetur necessitatibus labore perferendis omnis accusamus enim nihil repellat, eius in magni rem commodi expedita ducimus vero illum? Animi cupiditate molestias atque similique a. Officiis, mollitia odio. Qui quam perferendis deleniti iusto laudantium similique adipisci facere minima ipsam cum molestiae atque aut porro, omnis, accusamus quidem dolorum ipsa sequi pariatur, placeat dicta. Quia sint dolorum aspernatur dolores praesentium nulla, voluptatem facere quibusdam, sed perspiciatis rerum accusantium quos? Culpa totam sit aspernatur perspiciatis non id ducimus. Nulla nisi, possimus facilis minima vero totam? Accusamus ipsam optio corporis voluptate consequuntur natus, esse veniam repellat vitae ratione beatae repellendus, vero quia officiis labore tempora. At officia aspernatur esse recusandae asperiores tenetur alias enim sed, praesentium architecto assumenda debitis incidunt quo illo rerum harum ea? Labore harum fugiat facere, tempore dolor dolorem expedita repellendus at non quos culpa incidunt provident velit reprehenderit laudantium eos nemo nihil saepe asperiores fugit maiores ut. Voluptas commodi quas alias laudantium dignissimos, quam quia rem a in sunt consequuntur non dolore libero architecto excepturi ab ducimus. Modi blanditiis vel, voluptatibus sit quidem soluta minus! Aliquid nemo non unde illo molestias sed dignissimos nesciunt commodi deserunt beatae voluptates facilis totam pariatur, laboriosam qui molestiae dicta nostrum sequi. Doloremque architecto perferendis necessitatibus porro eligendi nobis recusandae. Accusantium magnam at vero, temporibus quia corrupti unde quo reiciendis quae obcaecati inventore ducimus nisi nobis deserunt officiis exercitationem ad quos perferendis suscipit optio veniam nulla cumque maxime sequi. Dignissimos animi reprehenderit earum nemo aspernatur eligendi quia doloremque totam distinctio perferendis officiis eum amet, sit quod nihil neque iste, ullam labore obcaecati voluptatem est modi? Laboriosam, asperiores corrupti eos molestiae ea similique voluptates maiores nesciunt ducimus aspernatur non quae dignissimos saepe perferendis voluptatem nihil facilis dicta accusantium aperiam nemo esse rem numquam impedit! Minima ad rerum, facere nobis repellat labore saepe amet assumenda a, maiores ducimus eos error cum tenetur sit? Saepe atque natus expedita fugiat voluptatum tempora nostrum inventore repellat praesentium consequuntur. Saepe quasi autem neque dolorem, tempore libero iusto officiis alias error, quia animi iure voluptas enim velit vero tenetur vel quis ut itaque qui, quos impedit id! Odit maiores rem nobis libero numquam et delectus facilis a, reprehenderit incidunt illum commodi asperiores mollitia corporis perspiciatis eum saepe doloremque optio accusantium cum adipisci minima amet inventore. Sit ullam sequi omnis consectetur ex culpa reprehenderit minima dolorem dignissimos, porro vitae id impedit cumque eveniet possimus neque eius molestias quae blanditiis nihil magnam nemo debitis repellat. Nisi magnam ipsa dolores quam in corporis dolore deserunt tempora eos nihil, repellat distinctio? Itaque, aut doloribus in laborum, ipsa accusamus delectus autem quis vel libero at, facilis quasi dicta dolor consequuntur sunt nulla atque cupiditate eius recusandae tempora nemo eaque culpa quam! Unde, sapiente maiores aperiam inventore quia, quidem laborum libero voluptas qui voluptatibus, quo itaque? At nihil pariatur voluptatem obcaecati laudantium voluptatum quibusdam explicabo tempore earum eius, voluptate, ipsa neque unde, dolorum dolorem? Odio numquam exercitationem minima autem tenetur quae eius, dolorem quos error repellendus quis maxime nesciunt? Dicta blanditiis molestias qui laboriosam non nobis quisquam voluptates aperiam provident, repellendus, ratione optio, consequatur ad dolores consequuntur facere soluta obcaecati corporis impedit odit ullam error. Possimus, a quos excepturi non, odio id, accusantium doloremque corrupti labore quisquam quo! Reprehenderit veritatis tempore facere! Blanditiis doloremque consectetur enim quis obcaecati praesentium, beatae, quod aperiam ipsa dolorum distinctio! Perspiciatis sed alias, a error nulla esse dolorem ratione nobis sunt illo soluta neque dignissimos optio ducimus modi excepturi accusamus officia molestias quibusdam blanditiis libero. Magni repellat, deleniti, corporis modi veniam fugiat commodi, id dolor sed minima nulla consequatur rem! Itaque vero, delectus laborum sit maxime tenetur quam fugit tempore ducimus asperiores suscipit exercitationem quae! Et iure consequatur necessitatibus culpa consequuntur tenetur ratione sapiente quo. Totam voluptas deleniti dolores iste voluptate facilis.</section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  background-image: url("../assets/hero-bg.png");
+  background-attachment: fixed;
+  position: relative;
+  height: 100vh;
+
+  img {
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+  }
+
+  .hero-text {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: #fff;
+
+    h4 {
+      text-transform: uppercase;
+      font-size: 22px;
+      padding-bottom: 4px;
+    }
+
+    h2 {
+      font-size: 50px;
+      @media (min-width: 550px) {
+        font-size: 80px;
+      }
+    }
+
+    hr:nth-child(2) {
+      max-width: 365px;
+      margin-bottom: 16px;
+    }
+    hr:nth-child(4) {
+      height: 6px;
+      background-color: #fff;
+      border: none;
+      max-width: 85px;
+      margin-top: 16px;
+    }
+  }
+}
+</style>
